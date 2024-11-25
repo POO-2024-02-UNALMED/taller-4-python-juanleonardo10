1a32760cee95cb5721ef93adf82eac9c879465dd
juanleonardo10 <159485131+juanleonardo10@users.noreply.github.com>
2024-11-25 02:00:15 +0000

    Melisimo

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,30 +3,26 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = None
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="Grupo ordinario", asignaturas=None, estudiantes=None):
         self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+        self._asignaturas = asignaturas if asignaturas is not None else []
+        self.listadoAlumnos = estudiantes if estudiantes is not None else []
 
-    def listadoAsignaturas(self, kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
+    def listadoAsignaturas(self, asignaturas_dict):
+        for nombre, salon in asignaturas_dict.items():
+            self._asignaturas.append(Asignatura(nombre, salon))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
+        if lista is None:
+            lista = []
         lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+        self.listadoAlumnos += lista
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        asignaturas_str = ', '.join([str(asig) for asig in self._asignaturas])
+        alumnos_str = ', '.join(self.listadoAlumnos)
+        return f"Grupo: {self._grupo}\nAsignaturas: {asignaturas_str}\nAlumnos: {alumnos_str}"
 
-    @ classmethod
+    @classmethod
     def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
