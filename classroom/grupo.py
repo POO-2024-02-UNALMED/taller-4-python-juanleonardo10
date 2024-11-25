92da8f99d4ef6306c7cbc95dea69adbac33f0e9c
juanleonardo10 <159485131+juanleonardo10@users.noreply.github.com>
2024-11-25 02:08:11 +0000

    Erdaa

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,22 +1,24 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = "Grado 6"  # Valor inicial por defecto
+    grado = "Grado 6"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
+    def _init_(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas if asignaturas else []
         self.listadoAlumnos = estudiantes if estudiantes else []
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
+        if lista is None:
+            lista = []
         lista.append(alumno)
-        self.listadoAlumnos.extend(lista)  # Agregar los nuevos alumnos a la lista
+        self.listadoAlumnos.extend(lista)
 
     def listadoAsignaturas(self, **kwargs):
         for nombre in kwargs.values():
-            self._asignaturas.append(Asignatura(nombre))  # Crear asignaturas con los nombres pasados
+            self._asignaturas.append(Asignatura(nombre))
 
-    def __str__(self):
+    def _str_(self):
         alumnos_str = ", ".join(self.listadoAlumnos)
         return f"Grupo de estudiantes: {self._grupo} ({alumnos_str})"
 
