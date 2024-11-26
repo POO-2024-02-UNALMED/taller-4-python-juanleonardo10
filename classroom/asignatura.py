2c86007309404c2f0310f9b2490d40f177fd95e5
juanleonardo10 <159485131+juanleonardo10@users.noreply.github.com>
2024-11-26 03:31:14 +0000

    ehg

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,4 +1,4 @@
-class asignatura:
+class Asignatura:
     def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,24 +1,25 @@
-class grupo:
-    grado = "Grado 6"
+from classroom.asignatura import Asignatura
+
+class Grupo:
+    grado = "Grado 12"
 
     def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
-        self._asignaturas = asignaturas if asignaturas else []
-        self.listadoAlumnos = estudiantes if estudiantes else []
+        self._asignaturas = asignaturas if asignaturas is not None else []
+        self.listadoAlumnos = estudiantes if estudiantes is not None else []
+
+    def listadoAsignaturas(self, **kwargs):
+        for nombre in kwargs.values():
+            self._asignaturas.append(Asignatura(nombre))
 
     def agregarAlumno(self, alumno, lista=None):
         if lista is None:
             lista = []
         lista.append(alumno)
-        self.listadoAlumnos.extend(lista)
-
-    def listadoAsignaturas(self, **kwargs):
-        for nombre in kwargs.values():
-            self._asignaturas.append(asignatura(nombre))
+        self.listadoAlumnos += lista
 
     def __str__(self):
-        alumnos_str = ", ".join(self.listadoAlumnos)
-        return f"Grupo de estudiantes: {self._grupo} ({alumnos_str})"  # Este formato debe ser exacto
+        return f"Grupo de estudiantes: {self._grupo}"
 
     @classmethod
     def asignarNombre(cls, nombre="Grado 6"):
